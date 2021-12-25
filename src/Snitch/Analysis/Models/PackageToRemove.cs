612ab46205417786cc2c597c2a7bceac858bacc1
Joseph Petersen <josephp90@gmail.com>
2021-12-25 22:47:43 +0100

    remove unused usings

--- src/Snitch/Analysis/Models/PackageToRemove.cs
+++ src/Snitch/Analysis/Models/PackageToRemove.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Diagnostics;
 
-using NuGet.Versioning;
-
 namespace Snitch.Analysis
 {
     [DebuggerDisplay("{PackageDescription(),nq}")]
--- src/Snitch/Analysis/Models/ProjectComparer.cs
+++ src/Snitch/Analysis/Models/ProjectComparer.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Snitch.Analysis
