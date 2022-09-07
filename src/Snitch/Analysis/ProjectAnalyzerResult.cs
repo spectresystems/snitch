07c7c6e110a5b38409163713145b54c7e5b834d2
Kerwin Carpede <kerwin.carpede@derivco.co.za>
2022-09-07 16:14:34 +0200

    feat: Added implementation for the --no-prerelease check

--- src/Snitch/Analysis/ProjectAnalyzerResult.cs
+++ src/Snitch/Analysis/ProjectAnalyzerResult.cs
@@ -6,21 +6,27 @@ namespace Snitch.Analysis
 {
     internal sealed class ProjectAnalyzerResult
     {
+        private readonly Project _project;
         private readonly List<PackageToRemove> _packages;
 
-        public string Project { get; }
+        public string Project => _project.Name;
         public IReadOnlyList<PackageToRemove> CanBeRemoved { get; }
         public IReadOnlyList<PackageToRemove> MightBeRemoved { get; }
 
+        public IReadOnlyList<Package> PreReleasePackages { get; }
+
         public bool NoPackagesToRemove => CanBeRemoved.Count == 0 && MightBeRemoved.Count == 0;
 
-        public ProjectAnalyzerResult(string project, IEnumerable<PackageToRemove> packages)
+        public bool HasPreReleases => PreReleasePackages.Count == 0;
+
+        public ProjectAnalyzerResult(Project project, IEnumerable<PackageToRemove> packages)
         {
-            Project = project;
+            _project = project;
             _packages = new List<PackageToRemove>(packages ?? throw new ArgumentNullException(nameof(packages)));
 
             CanBeRemoved = new List<PackageToRemove>(packages.Where(p => p.CanBeRemoved));
             MightBeRemoved = new List<PackageToRemove>(packages.Where(p => p.VersionMismatch));
+            PreReleasePackages = new List<Package>(project.Packages.Where(p => p.Version != null && p.Version.IsPrerelease));
         }
 
         public ProjectAnalyzerResult Filter(string[]? packages)
@@ -31,7 +37,7 @@ namespace Snitch.Analysis
             }
 
             var filtered = _packages.Where(p => !packages.Contains(p.Package.Name, StringComparer.OrdinalIgnoreCase));
-            return new ProjectAnalyzerResult(Project, filtered);
+            return new ProjectAnalyzerResult(_project, filtered);
         }
     }
 }
